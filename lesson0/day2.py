13eac186c3774a70ffd6d7d41688ba0925700f73
katomoralishvili <moralishvilikato@gmail.com>
2024-04-07 23:58:01 +0400

    second commit

---
+++ lesson0/day2.py
@@ -0,0 +1,4 @@
+print("gavicani da davumegobrdi")
+print("dadiani datas da giorgi robakidzes")
+print("ras vfiqrob GOA ze")
+print("GOA me vtvli rom aris adgili,sadac nebismieri adamiani,nebismieri adgilidan shedzlebs kavshiris gamonaxvas vinme sxvastan.GOA aris uertiertobis,ganatlebis,pirovnuli ganvitarebistvis idealuri adgili.piradad me axali gaertianebuli var da vcdilob davewio jgufs,rac jer-jerobit rtulia,tumca minda viyo chadi da tu mizans davisaxav aucileblad mivagwev.dzalian didi imedi maqvs,rom xalxtan urtiertobis gzas martivad gamovnaxav.aucileblad dasafasebelia batoni nikas da misi tanagundelebis daugalavi shroma.ME MJERA,ROM GOA QARTVELI XALXIS GANVITAREBIS MTAVARI WYARO IQNEBA DA SURVILI MAQVS AM DIDI OJAXIS WEVRI GAVXDE.  ")

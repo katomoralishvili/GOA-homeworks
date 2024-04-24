7e452a3ad29cee2e88edd7b932827614dc039ca0
katomoralishvili <moralishvilikato@gmail.com>
2024-04-24 23:57:36 +0400

    lesson5.py

---
+++ lesson0/lesson5.py
@@ -0,0 +1,23 @@
+num1 = int(input("please enter first number: "))
+num2 = int(input("please enter second number: "))
+
+print(type(num1))
+print(type(num2))
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
+#second part of our homework
+print(input("please enter ok to start exercises: "))
+print(13 / 2)
+print(7 // 3)
+print(42 % 4)
+print(3 % 4)
+print(28 // 2)
+print(12 / 3)
+print(8 % 3)
+print(49 // 7)
+print(103 / 2)
+print(11 / 2)

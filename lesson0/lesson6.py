171c65881d8c0ba88ab4dd72e8a61324f8f83bce
katomoralishvili <moralishvilikato@gmail.com>
2024-05-06 19:22:59 +0400

    lesson6.py

---
+++ lesson0/lesson6.py
@@ -0,0 +1,73 @@
+num1 = int(input("please enter first number: " ))
+num2 = float(num1)
+print(type(num1) == type(num2))
+#printing examples for ">"
+print(3 > 2)
+print(19 > 9)
+print(7 > 90)
+print(999 >1)
+print(28 >5)
+
+#printing examples for ">="
+print(9 >=4 )
+print(7 >=99)
+print(973 >=2)
+print(2210 >=5239)
+print(2103 >=5555)
+
+#printing examples for "<"
+print(77777 < 555)
+print(522000<4)
+print(4<5)
+print(2<1)
+print(9<5)
+
+
+#printing examples for "<="
+print(201<=5)
+print(555<=3)
+print(6<=2)
+print(3333<=0)
+print(4<=5)
+
+#printing examples for "=="
+print(5==5)
+print(9==2)
+print(3==2)
+print(102==1)
+print(555555==3)
+
+#printing examples for "!="
+print(5!=2)
+print(9!=0)
+print(28!=3)
+print(16!=9)
+print(11111!=2)
+
+
+
+num3 = int(input("please enter your number: "))
+num4 = int(input("please enter your second number: "))
+
+print(num3 > num4)
+print(num3 < num4)
+print(num3 >= num4)
+print(num3 <= num4)
+print(num3 == num4)
+print(num3 != num4)
+
+num5 = int(input("please enter your number: "))
+
+print(num5 >num5*10)
+print(num5 <num5*10)
+print(num5 >=num5*10)
+print(num5 <=num5*10)
+print(num5 ==num5*10)
+print(num5 !=num5*10)
+
+
+num6 = int(input("please enter your number: "))
+num7 = float(input("please enter decimal number: "))
+
+print(num6 == num7)
+print(num6 != num7)

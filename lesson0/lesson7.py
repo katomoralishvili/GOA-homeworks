4d51ee622ac2af7d5c4abd6070352894b0d5c041
katomoralishvili <moralishvilikato@gmail.com>
2024-05-10 18:36:30 +0400

    lesson7.py

---
+++ lesson0/lesson7.py
@@ -0,0 +1,20 @@
+temp_in_celsius = 10
+temp_in_fahrenheit = (temp_in_celsius * 1.8) + 32
+ 
+print(temp_in_fahrenheit)
+
+#homework 2
+age = int(input("please enter your age: "))
+print(age > 12 and age < 20)
+
+#homework 3
+length = float(input("please enter rect length: "))
+width = float(input("please enter your rect width: "))
+
+result = length * width
+
+print(result)
+
+#homework 4 
+num =int(input("please enter your number: "))
+print(num>100 and num % 9)

16656d04f0c49443f582900db661dd92cea30fd9
katomoralishvili <moralishvilikato@gmail.com>
2024-05-23 21:16:39 +0400

    Day 09

---
+++ Day 09/homework/lesson9.py
@@ -0,0 +1,7 @@
+salary =int(input("please enter your salary: "))
+if salary>10000:
+    print("have you studied in GOA?:")
+elif 1000<salary<10000:
+    print("are you a mid?:")
+else:
+     print("you should have joined to GOA matrix")
